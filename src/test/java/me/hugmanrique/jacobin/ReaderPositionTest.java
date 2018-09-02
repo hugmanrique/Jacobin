8d67948fde8c72ada194942c1837687335039b14
Hugo Manrique <hugmanrique@gmail.com>
2018-09-02 22:45:48 +0200

    Add unsigned tests

--- src/test/java/me/hugmanrique/jacobin/ReaderPositionTest.java
+++ src/test/java/me/hugmanrique/jacobin/ReaderPositionTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.io.EOFException;
 import java.io.IOException;
-import java.nio.ByteOrder;
 
 import static org.junit.Assert.assertEquals;
 
---
+++ src/test/java/me/hugmanrique/jacobin/UnsignedTest.java
@@ -0,0 +1,30 @@
+package me.hugmanrique.jacobin;
+
+import me.hugmanrique.jacobin.util.Unsigned;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Hugo Manrique
+ * @since 02/09/2018
+ */
+public class UnsignedTest {
+    @Test
+    public void testUnsigned() {
+        assertEquals(
+            Short.toUnsignedInt(Short.MIN_VALUE),
+            Unsigned.unsignedInt16(Short.MIN_VALUE)
+        );
+
+        assertEquals(
+            Integer.toUnsignedLong(Integer.MIN_VALUE),
+            Unsigned.unsignedInt32(Integer.MIN_VALUE)
+        );
+
+        assertEquals(
+            Long.toUnsignedString(Long.MIN_VALUE),
+            Long.toUnsignedString(Unsigned.unsignedInt64(Long.MIN_VALUE))
+        );
+    }
+}
