1a2833e3790e172f6af86a8f1ac6f1acffe615e4
Hugo Manrique <hugmanrique@gmail.com>
2018-12-25 17:16:56 +0100

    Add negative write offsets tests

--- src/test/java/me/hugmanrique/jacobin/awesomerewrite/writer/DataWriterTest.java
+++ src/test/java/me/hugmanrique/jacobin/awesomerewrite/writer/DataWriterTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * @author Hugo Manrique
@@ -28,49 +28,23 @@ public class DataWriterTest {
 
         writer.skip(4);
         assertEquals("Skips larger than the stream's size should update the offset", 7, writer.getOffset());
-
-
-
-
-
-        /*
-        int[] bytes = new int[] { 0x12, 0x34, 0x56, 0x78 };
-
-        DataReader reader = new DataReader(
-            createInputStream(bytes)
-        );
-
-        assertEquals("Initial offset must be zero", 0, reader.getOffset());
-
-        reader.setOffset(2);
-        assertEquals("Offset after setting must be equal", 2, reader.getOffset());
-
-        reader.skip(1);
-        assertEquals("Skip must update the offset", 3, reader.getOffset());
-
-        reader.skip(-2);
-        assertEquals("Negative skip must update the offset", 1, reader.getOffset());
-
-        reader.setOffset(0);
-        assertEquals("Negative offset setting must update the offset", 0, reader.getOffset());
-
-        reader.skip(3);
-        reader.reset();
-
-        assertEquals("Resetting should set the offset to zero", 0, reader.getOffset());
-
-        int available = reader.available();
-        assertEquals("Available bytes must be equal to array size", bytes.length, available);
-
-        reader.skip(4);
-        assertEquals("Skips larger than the stream's size should update offset correctly", 4, reader.getOffset());
-         */
-
     }
 
-    @Test(expected = UnsupportedOperationException.class)
-    public void testNegativeSkip() {
+    @Test
+    public void testNegativeOffsets() throws IOException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream(2);
+        DataWriter writer = new DataWriter(stream);
 
+        // Move pointer to offset 1
+        writer.skip(1);
 
+        assertFalse("Default implementation does not support negative skips", writer.supportsNegativeSkips());
+
+        try {
+            writer.skip(-1);
+            writer.setOffset(0);
+
+            fail();
+        } catch (UnsupportedOperationException ignored) {}
     }
 }
