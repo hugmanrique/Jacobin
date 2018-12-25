e774b4cd5d599315a4ad798de34a8b7480ba01c9
Hugo Manrique <hugmanrique@gmail.com>
2018-12-25 15:54:00 +0100

    Add endianness-dependent DataReader tests

--- src/test/java/me/hugmanrique/jacobin/awesomerewrite/reader/BigEndianDataReaderTest.java
+++ src/test/java/me/hugmanrique/jacobin/awesomerewrite/reader/BigEndianDataReaderTest.java
@@ -11,32 +11,32 @@ import static org.junit.Assert.assertEquals;
  * @author Hugo Manrique
  * @since 25/12/2018
  */
-public class LittleEndianDataReaderTest {
+public class BigEndianDataReaderTest {
 
-    private LittleEndianDataReader createReader(int... bytesAsInts) {
-        return new LittleEndianDataReader(
+    private BigEndianDataReader createReader(int... bytesAsInts) {
+        return new BigEndianDataReader(
             createInputStream(bytesAsInts)
         );
     }
 
     @Test
     public void testNumberRead() throws IOException {
-        LittleEndianDataReader reader = createReader(0x34, 0x12);
+        BigEndianDataReader reader = createReader(0x12, 0x34);
         assertEquals((short) 0x1234, reader.readInt16());
 
-        reader = createReader(0xCD, 0xAB);
+        reader = createReader(0xAB, 0xCD);
         assertEquals((short) 0xABCD, reader.readInt16());
 
-        reader = createReader(0x78, 0x56, 0x34, 0x12);
+        reader = createReader(0x12, 0x34, 0x56, 0x78);
         assertEquals(0x12345678, reader.readInt32());
 
-        reader = createReader(0xF0, 0xDE, 0xBC, 0x9A);
+        reader = createReader(0x9A, 0xBC, 0xDE, 0xF0);
         assertEquals(0x9ABCDEF0, reader.readInt32());
 
-        reader = createReader(0xF0, 0xDE, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12);
+        reader = createReader(0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0);
         assertEquals(0x123456789ABCDEF0L, reader.readInt64());
 
-        reader = createReader(0x78, 0x56, 0x34, 0x12, 0xF0, 0xDE, 0xBC, 0x9A);
+        reader = createReader(0x9A, 0xBC, 0xDE, 0xF0, 0x12, 0x34, 0x56, 0x78);
         assertEquals(0x9ABCDEF012345678L, reader.readInt64());
     }
 }
